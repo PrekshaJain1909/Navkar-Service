63f41c90ba1d1abbd45c17ed2a3dbdfef406e1a3
Preksha Jain <Preksha190994@gmail.com>
2025-08-16 16:01:15 +0530

    deleted frontend

--- frontend/app/tracker/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '@/components/dashboard-layout'
-
-export default function TrackerLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>
-}
